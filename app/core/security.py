2d09446a414160e88ea5d98faa7272a932df07b5
NumiViX <yakhin.v.r@yandex.ru>
2025-07-07 16:37:08 +0500

    Fix token parameter naming

--- app/core/security.py
+++ app/core/security.py
@@ -19,9 +19,9 @@ def get_password_hash(password):
     return pwd_context.hash(password)
 
 
-def create_access_token(data: dict, expides_delta: timedelta | None = None):
-    to_encode_date = data.copy()
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode_data = data.copy()
     expired_time = (datetime.now(timezone.utc) +
-                    (expides_delta or timedelta(minutes=15)))
-    to_encode_date.update({"exp": expired_time})
-    return jwt.encode(to_encode_date, SECRET_KEY, algorithm=ALGORITHM)
+                    (expires_delta or timedelta(minutes=15)))
+    to_encode_data.update({"exp": expired_time})
+    return jwt.encode(to_encode_data, SECRET_KEY, algorithm=ALGORITHM)
